3615416607bb209a9068e48055f3fd899af7de09
ikbaladisaefuloh27 <148667436+ikbaladisaefuloh27@users.noreply.github.com>
2023-11-08 10:50:38 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function Login() {
+    var done = 0;
+    var username = documen.login.username.value;
+    var password = documen.login.username.value;
+}
+if(username == "himasi" && password == "himasi") {
+    if(confirm("apakah ingin lo0gin") == true) {
+        window.location.href ="https://github.com/ikbaladisaefuloh27";
+    }
+    else{
+        alert ("membatalkan login!");
+    } 
+} 
+    else {
+    if(done==0){
+        alert("username/password salah:")
+    }
+}
